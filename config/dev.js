08176aba69c5d8d5bcd9a9f8f958caaa9bce06c0
zabhitak <zabhitak1292000@gmail.com>
2020-07-02 18:58:58 +0530

    portfolio

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    GMAIL_USER:'zabhi1292@gmail.com',
+    GMAIL_PASS:'zabhitak15'
+};
+
+//mongodb+srv://zabhitak:zabhitak@cluster0-ikcgk.mongodb.net/Cluster0?retryWrites=true&w=majority
+
+//mongodb+srv://zabhitak:zabhitak@cluster0-yh6sk.mongodb.net/Cluster0?retryWrites=true&w=majority
+
