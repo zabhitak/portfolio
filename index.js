cbe287db602c551a3c9d3ee56b5504401099825b
zabhitak <zabhitak1292000@gmail.com>
2020-07-15 19:47:04 +0530

    abhinav

--- index.js
+++ index.js
@@ -38,9 +38,31 @@ app.post('/contact', (req, res) => {
     from: req.body.email,
     to: keys.GMAIL_USER,
     subject: 'PortFolio',
-    text: `Heyy Utkrisht, I am ${req.body.name} (${req.body.email}) this side ,just go through your Portfolio and wanna say: ${req.body.message}`
+    text: `Heyy Abhinav, I am ${req.body.name} (${req.body.email}) this side ,just go through your Portfolio and wanna say: ${req.body.message}`
   }
   smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      res.render('contact') 
+    }
+  })
+
+
+  const smtpTransReply = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: keys.GMAIL_USER,
+      pass: keys.GMAIL_PASS
+    }
+  })
+  const mailOpts1 = {
+    from: keys.GMAIL_USER,
+    to: req.body.email,
+    subject: 'PortFolio',
+    text: `Heyy ${req.body.name}, I am Abhinav this side, thankyou for going through my portfolio and I will surely revert back you if there is any query `
+  }
+  smtpTransReply.sendMail(mailOpts1, (error, response) => {
     if (error) {
       res.render('contact') 
     }
