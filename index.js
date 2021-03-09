1f8ff1bfb6f2a0251ff7508d76af706dc3fd5aa2
zabhitak <54598293+zabhitak@users.noreply.github.com>
2021-03-09 21:18:31 +0530

    abhi

--- index.js
+++ index.js
@@ -86,5 +86,5 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started at port ${port}`));
