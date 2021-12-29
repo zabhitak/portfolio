16aa7ed4d10cceba49b0811ea3dacbfbd612fbd5
zabhitak <zabhi1292@gmail.com>
2021-12-29 22:39:23 +0530

    updates

---
+++ Readme.md
@@ -0,0 +1,10 @@
+<div align="center">
+    <h1>Abhinav Goyal</h1>
+    <i>Everything you want to know about me -</i>
+    <br target="_blank">https://zabhi.herokuapp.com//<br>
+    </div>
+   <p align="center">
+ <img src="https://github.com/zabhitak/portfolio_node/blob/master/abhi.gif" align="center" height="400px;"width="500px; alt=""/>
+</p>
+    <sub><sup>© 2020 Abhinav Portfolio, licensed under the <a href="./LICENSE">MIT License</a>.</sup></sub>
+
---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const compression = require('compression');
+const helmet = require('helmet');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('public'));
+app.use(compression());
+app.use(helmet());
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/contact', (req, res) => {
+  const smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  })
+  const mailOpts = {
+    from: req.body.email,
+    to: process.env.GMAIL_USER,
+    subject: 'PortFolio',
+    text: `Heyy Abhinav, I am ${req.body.name} (${req.body.email}) this side ,just go through your Portfolio and wanna say: ${req.body.message}`
+  }
+  smtpTrans.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      res.render('contact') 
+    }
+  })
+
+
+  const smtpTransReply = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  })
+  const mailOpts1 = {
+    from: process.env.GMAIL_USER,
+    to: req.body.email,
+    subject: 'PortFolio',
+    text: `Heyy ${req.body.name}, I am Abhinav this side, thankyou for going through my portfolio and I will surely revert back you if there is any query `
+  }
+  smtpTransReply.sendMail(mailOpts1, (error, response) => {
+    if (error) {
+      res.render('contact') 
+    }
+  })
+});
+
+
+app.get('/gallery', (req, res) => {
+  res.render('gallery');
+});
+
+
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio');
+});
+
+
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started at port ${port}`));
